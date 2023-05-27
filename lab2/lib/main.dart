07cd82f7f7d4b954c08c5b12f518c592a14affb1
Heilen99 <heilen.segura.cordero@est.una.ac.cr>
2023-05-26 18:38:15 -0600

    Correcciones, eliminación de clases y creación de otras clases en el main

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'ImageList.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+const apiKey = '36734116-1c72c0b193012e7687e321172';
 
 void main() {
   runApp(MyApp());
@@ -13,11 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image List',
+      title: 'Image Gallery',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImageList(),
+      home: ImageGallery(),
     );
   }
+}
+
+class ImageGallery extends StatefulWidget {
+  @override
+  _ImageGalleryState createState() => _ImageGalleryState();
 }
